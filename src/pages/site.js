509c3e34d141b7f96483274d30ea7344d990fc51
emanuelkoseos <emanuel.koseos@gmail.com>
2022-09-26 23:49:02 -0700

    updates to site.js

--- src/pages/site.js
+++ src/pages/site.js
@@ -4,10 +4,7 @@ import { navigate } from 'gatsby';
 
 export default function Site() {
     if (typeof window !== 'undefined') {
-        // redirect after 1sec
-        //window.setTimeout(function() {
-          window.location.pathname = '/site/index.html'
-        //}, 1000)
+          window.location.pathname = '/site/index.html';
       }
     return null;
 
