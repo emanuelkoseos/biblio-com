6b55bf5bd50c0566d9859348a9d6348bbbb09f38
emanuelkoseos <emanuel.koseos@gmail.com>
2022-09-27 03:02:59 -0700

    update route 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { navigate } from 'gatsby';
 
 export default function Home() {
   if (typeof window !== 'undefined') {
-        window.location.href = '/site/index.html';
+        window.location.href = 'site/index.html';
     }
     return <div>
     <h3>loading...please wait.</h3>
